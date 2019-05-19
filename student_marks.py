d5a71fb349313d917a9366a2885fed9d39bea3c8
sonamg111 <sonamgoud713@gmail.com>
2019-05-19 11:02:43 +0530

    chenge this file

---
+++ student_marks.py
@@ -0,0 +1,7 @@
+student_marks=[23,45,89,90,56,80]
+lenght=len(student_marks)
+index=0
+total_marks=0
+while index<len(student_marks):
+	total_marks=student_marks[index]+total_marks
+print"total marks:"+str(total_marks)	
